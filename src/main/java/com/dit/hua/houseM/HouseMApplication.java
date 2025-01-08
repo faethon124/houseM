e00719d005b3d8b28d20640a3bb1da087d27b8b1
faethon124 <it2021124@hua.gr>
2025-01-08 12:59:54 +0200

    Initial commit

---
+++ src/main/java/com/dit/hua/houseM/HouseMApplication.java
@@ -0,0 +1,13 @@
+package com.dit.hua.houseM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dit/hua/houseM/HouseMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.hua.houseM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
