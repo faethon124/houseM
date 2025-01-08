966c3650838fe219fa3d513c0f85366a2f6367c3
ΣΤΥΛΙΑΝΟΣ ΤΣΟΠΑΝΟΓΛΟΥ ΒΛΑΣΑΚΙΔΗΣ <92670770+tsopanogloustelios@users.noreply.github.com>
2025-01-08 15:52:48 +0200

    rdcrdcrdc

---
+++ src/main/java/com/dit/hua/houseM/entities/ApplicationForm.java
@@ -0,0 +1,154 @@
+package com.dit.hua.houseM.entities;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name="applicationform")
+public class ApplicationForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="TPN")
+    private String TPN;
+
+    @Column(name="date")
+    private  Date  date;
+
+    @Column(name="katoikia")
+    private String katoikia;
+
+    public ApplicationForm(int id, String firstName, String lastName, String email, String phone, String username, String password, String TPN, Date date, String katoikia) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+        this.TPN = TPN;
+        this.date = date;
+        this.katoikia = katoikia;
+    }
+
+    public ApplicationForm() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTPN() {
+        return TPN;
+    }
+
+    public void setTPN(String TPN) {
+        this.TPN = TPN;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getKatoikia() {
+        return katoikia;
+    }
+
+    public void setKatoikia(String katoikia) {
+        this.katoikia = katoikia;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationForm{" +
+                "id=" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", TPN='" + TPN + '\'' +
+                ", date=" + date +
+                ", katoikia='" + katoikia + '\'' +
+            '}';
+    }
+}
+
